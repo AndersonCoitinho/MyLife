1428e3c394f5dfc31830bf7988bd768fa39034cd
Anderson Coitinho <andercoitinho@gmail.com>
2022-12-03 21:44:45 -0300

    Inserindo SelectHabit, Dropdown e Database

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -6,13 +6,17 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
-  Alert
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
-export default function HabitPage() {
+import SelectHabit from "../../Components/HabitPage/SelectHabit";
+
+export default function HabitPage({ route }) {
 	const navigation = useNavigation();
+	const [habitInput, setHabitInput] = useState();
+
+	const { create, habit } = route.params;
 
   return (
     <View style={styles.container}>
@@ -28,7 +32,14 @@ export default function HabitPage() {
             />
           </TouchableOpacity>
           <View style={styles.mainContent}>
-            
+            <Text style={styles.title}>Configurações {"\n"} de hábito</Text>
+            <Text style={styles.inputText}>Área</Text>
+            <View style={styles.inputContainer}>
+              <Text style={styles.area}>{habit?.habitArea}</Text>
+            </View>
+
+            <Text style={styles.inputText}>Hábito</Text>
+            <SelectHabit habit={habit} habitInput={setHabitInput} />
           </View>
         </View>
       </ScrollView>
@@ -54,4 +65,28 @@ const styles = StyleSheet.create({
     width: 250,
     alignSelf: "center",
   },
-})
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+  },
+  inputText: {
+    color: "white",
+    fontSize: 16,
+    marginTop: 35,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  area: {
+    color: "#BBBBBB",
+    fontSize: 15,
+  },
+});
